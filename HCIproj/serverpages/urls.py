dd097e0047e7819737d3da3d176d47fe163c5a8a
ColdFerrin <ferrinkatz@ferrinkatz.com>
2019-12-05 14:26:47 -0700

    add some basic api endpoints

--- HCIproj/serverpages/urls.py
+++ HCIproj/serverpages/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),        
+    path('pos/', views.getPos, name="Get Position"),
+    path('pos/<slug:slug>/', views.setHome, name="Set Home"),
 ]
--- HCIproj/serverpages/views.py
+++ HCIproj/serverpages/views.py
@@ -1,6 +1,46 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import json
 # Create your views here.
+
+data = {}
+data['home'] = 'none';
+data['xpos'] = 
+
+with open('data.txt', 'w') as outfile:
+    json.dump(data, outfile)
+
+pos = {}
 
 def index(request):
     return HttpResponse('hello world')
+
+def getPos(request):
+    if request.method == 'GET':
+        a = None
+        with open('data.txt') as infile:
+            a = json.load(infile)
+        if a == {}:
+            responseData = {}
+            responseData['code'] = 400
+            responseData['message'] = 'No Base Airport'
+            
+            response = HttpResponse(json.dumps(responseData),
+            'application/json', 400)
+            return response;
+        else:
+        
+
+            return JsonResponse({'no':'no'})  
+
+@csrf_exempt
+def setHome(request, slug):
+    if request.method == 'PUT':
+        a = slug
+        data = {}
+        data['home'] = slug
+        with open('data.txt', 'w') as outfile:
+            json.dump(data, outfile)
+        return JsonResponse(data)
+
